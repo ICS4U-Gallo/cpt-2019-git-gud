b0fe924408657d68198fa3681b67164fe61fe570
Joshua Chuang <joshua.chuang21@ycdsbk12.ca>
2019-12-23 16:12:05 -0500

    menu background

--- menu.py
+++ menu.py
@@ -5,16 +5,21 @@ import settings
 
 class MenuView(arcade.View):
     def on_show(self):
-        arcade.set_background_color(arcade.color.WHITE)
+        arcade.set_background_color(arcade.color.BLACK)
+        self.background = arcade.load_texture(
+            "images/screen_menu_background.png")
 
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_text("Pokemon PC", settings.WIDTH/2, settings.HEIGHT/2,
-                         arcade.color.BLACK, font_size=30, anchor_x="center")
+        
+        
+        arcade.draw_texture_rectangle(settings.WIDTH // 2, settings.HEIGHT // 2,
+                                      settings.WIDTH, settings.HEIGHT, self.background)
 
     def on_key_press(self, key, modifiers):
-        self.director.next_view()
-
+        if key == arcade.key.ENTER:
+            self.director.next_view()
+            
 
 if __name__ == "__main__":
     """This section of code will allow you to run your View
