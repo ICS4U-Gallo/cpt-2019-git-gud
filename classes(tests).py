e37ba0668a3608f61a7c7e30140e70081dd5b685
Joseph <jzhang.joe03@gmail.com>
2020-01-09 13:09:35 -0500

    update

--- classes(tests).py
+++
@@ -1,9 +0,0 @@
-class Player(Pokemon):
-    def __init__(self, name, health_points, Type, passive_ability, moveset, image, level=1):
-        super().__init__(name, health_points, Type, passive_ability, moveset, image, level=level)
-        x = 0
-        y = 0
-
-class Shadow(Pokemon):
-    def __init__(self, name, health_points, Type, passive_ability, moveset, image, level=1):
-        super().__init__(name, health_points, Type, passive_ability, moveset, image, level=level)
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import json
 
-i = {"Name": "???", "Gender": "Male"}
+i = {"Name": "???", "Gender": "???", "Money": 0, "Pokemons": {}, "Inventory": {}, "Location": "view"}
 
 with open("saves\journey_1.json", "w") as f:
     json.dump(i, f, indent=4)
